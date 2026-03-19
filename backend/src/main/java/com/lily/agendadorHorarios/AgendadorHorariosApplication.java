6de3213f2ff55c85ce0344273bda00d26b6cf4e1
Lídia Torres <109477475+lilytorrx@users.noreply.github.com>
2026-03-19 12:14:26 -0300

    fix: adjusting dependencies for deploy

--- backend/src/main/java/com/lily/agendadorHorarios/AgendadorHorariosApplication.java
+++ backend/src/main/java/com/lily/agendadorHorarios/AgendadorHorariosApplication.java
@@ -10,14 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class AgendadorHorariosApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure()
-				.directory(System.getProperty("user.dir"))
-				.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("SECRET_KEY", dotenv.get("SECRET_KEY"));
-
 		SpringApplication.run(AgendadorHorariosApplication.class, args);
 	}
 }
