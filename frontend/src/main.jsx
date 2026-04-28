13739dd2e1514998c9af89e98f3142028a49b56c
Lídia Torres <109477475+lilytorrx@users.noreply.github.com>
2026-04-28 15:05:30 -0300

    feat: adding services for call to API

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext.jsx"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+      <BrowserRouter>
+        <AuthProvider>
+          <App/>
+        </AuthProvider>
+      </BrowserRouter>
+    </React.StrictMode>
 );
